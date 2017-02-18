3949e6338eb8272dbf2eee216572acadca3cceda
Pieter Mathys <pieter.mathys@dazzle.be>
2017-02-18 15:00:27 +0100

    Init repo

---
+++ js/d8imd.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Example drupal behavior
+   */
+/*  Drupal.behaviors.exampleBehavior = {
+    attach: function (context, settings) {
+      $('.example', context).once('example-behavior').each(function () {
+
+      });
+    }
+  };*/
+
+})(jQuery, Drupal);
+
